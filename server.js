0b728072e846961bc8d68a22b530ce8d0476905a
tazera <tazera@abv.bg>
2021-04-01 22:23:22 +0100

    attempting heroku deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const utils = require('./utils');
-const path = require('path')
+const path = require('path');
 
 
 
